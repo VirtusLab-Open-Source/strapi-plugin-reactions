de37721a92deb2183c52fe998e9ed0ebccff5f1e
Mateusz Ziarko <mateusz.ziarko@webninja.eu>
2024-06-13 12:12:24 +0200

    fix: setup package fix

--- setup-package.ts
+++ setup-package.ts
@@ -10,13 +10,13 @@
 import fs from "fs";
 
 // This script is called from within the build folder. It is important to include it in .npmignore, so it will not get published.
-const sourceDirectory = __dirname + "/..";
-const destinationDirectory = __dirname;
+const sourceDirectory = __dirname;
+const destinationDirectory = __dirname + "/dist";
 
 function main() {
   // Generate publish-ready package.json
   const source = fs
-    .readFileSync(__dirname + "/../package.json")
+    .readFileSync(__dirname + "/package.json")
     .toString("utf-8");
   const sourceObj = JSON.parse(source);
   sourceObj.scripts = {};
